efa4aab9f1d02d62875b2521c36d2c1fd703b6be
theomilll <tam4@cesar.school>
2022-12-09 12:58:49 -0300

    codigo funcionando'

--- q1_tam4.c
+++ q1_tam4.c
@@ -2,6 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+int wellDefinedString(char *A){
+    int i;
+    int count1 = 0;
+    int count2 = 0;
+    int count3 = 0;
+    for(i = 0; i < strlen(A); i++){
+        if(A[i] == '('){
+            count1++;
+        }
+        else if(A[i] == ')'){
+            count1 = count1 - 1;
+        }
+        if(A[i] == '['){
+            count2++;
+        }
+        else if(A[i] == ']'){
+            count2 = count2 - 1;
+        }
+        if(A[i] == '{'){
+            count3++;
+        }
+        else if(A[i] == '}'){
+            count3 = count3 - 1;
+        }
+    }
+    if(count1 == 0 && count2 == 0 && count3 == 0){
+        return 1;
+    }
+    return 0;
+}
 
+int main(){
+    char A[1000000];
+    int T;
+    int result;
+    scanf("%d", &T);
+    if(T <= 1 || T <= 20){
+        for (int i = 0; i < T; i++){
+            scanf("%s", A);
+            result = wellDefinedString(A);
+            if(result == 1){
+                printf("S\n");
+            }
+            else{
+                printf("N\n");
+            }
+        }
+    }
 }
